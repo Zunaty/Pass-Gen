adc1beb2a52fcd44e0ba210a7c7c7b127d830fd2
Victor Perez <zunaty@gmail.com>
2021-07-03 12:01:46 -0600

    initial setup

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,8 +1,11 @@
-// Assignment code here
-
-
+// Array holding the alphabet
+var alphabet = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
+
+function generatePassword() {
+
+};
 
 // Write password to the #password input
 function writePassword() {
@@ -10,8 +13,7 @@ function writePassword() {
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
-
-}
+};
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
